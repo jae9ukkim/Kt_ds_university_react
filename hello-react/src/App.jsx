d258a04c56be0836e6c0fd2a34d6c7fed8d3759c
jae9ukkim <jaegogjaegog@gmail.com>
2026-04-23 17:49:37 +0900

    assign

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,8 +1,9 @@
 /** @format */
 // 다른 폴더에 있는 component 가져오는 방법
 import ArticleMain from "./components/articles/ArticleMain.jsx";
+import { Counter } from "./components/assignment/counter.jsx";
 import TodoMain from "./components/todo/TodoMain.jsx";
 
 export default function App() {
-  return <TodoMain />;
+  return <Counter />;
 }
---
+++ hello-react/src/components/assignment/Counter.jsx
@@ -0,0 +1,12 @@
+export const Counter = () => {
+  const MIN_VAL = 0;
+  const MAX_VAL = 100;
+  const count = 0;
+  return (
+    <div className="wrapper">
+      <button>-</button>
+      <div></div>
+      <button>+</button>
+    </div>
+  );
+};
